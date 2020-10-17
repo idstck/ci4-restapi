7c7c0cec2e6311687aa596e7c65aca4bcb075f7e
Lunadiotic <hakimsembiring@outlook.co.id>
2020-10-18 02:45:17 +0700

    membuat model, controller dan view json dengan route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,8 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+$routes->get('contacts', 'Contact::index');
+
 /**
  * --------------------------------------------------------------------
  * Additional Routing
---
+++ app/Models/ContactModel.php
@@ -0,0 +1,9 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ContactModel extends Model
+{
+    protected $table = 'contacts';
+    protected $primaryKey = 'id';
+}
